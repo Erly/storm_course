d3990a954cd377a3939e8672edc699bd1404ae05
Erlantz Oniga <erlantz8@gmail.com>
2016-11-28 13:27:24 +0100

    Added bolt id constants

--- src/main/java/com/erlantzoniga/storm_course/utils/Constants.java
+++ src/main/java/com/erlantzoniga/storm_course/utils/Constants.java
@@ -1,6 +1,9 @@
 package com.erlantzoniga.storm_course.utils;
 
 public class Constants {
+  public static final String BOLT_SPLIT_SENTENCE = "SPLIT_SENTENCE_BOLT";
+  public static final String BOLT_WORD_COUNT= "WORD_COUNT_BOLT";
+
   public static final String TWEET_ID = "TWEET_ID";
   public static final String TWEET_TEXT = "TWEET_TEXT";
   public static final String TWEET_METADATA = "TWEET_METADATA";
