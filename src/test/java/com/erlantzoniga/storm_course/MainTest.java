8afb261b41b86c7241c121783a2346333b474593
Erlantz Oniga <erlantz8@gmail.com>
2017-01-10 21:11:02 +0100

    Added test class and AssertJ dependency

---
+++ src/test/java/com/erlantzoniga/storm_course/MainTest.java
@@ -0,0 +1,17 @@
+package com.erlantzoniga.storm_course;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+  @Before
+  public void setUp() throws Exception {
+
+  }
+
+  @Test
+  public void main() throws Exception {
+
+  }
+
+}
