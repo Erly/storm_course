2522dbd95c8739d636cb33ced6f096d76b2b9c0a
Erlantz Oniga <erlantz.oniga@veinteractive.com>
2017-01-27 09:58:33 +0100

    Added topology name and configuration parameter names to constants

--- src/main/java/com/erlantzoniga/storm_course/utils/Constants.java
+++ src/main/java/com/erlantzoniga/storm_course/utils/Constants.java
@@ -1,6 +1,8 @@
 package com.erlantzoniga.storm_course.utils;
 
 public class Constants {
+  public static final String TOPOLOGY_NAME = "STORM_COURSE_TOPOLOGY";
+
   public static final String SPOUT_RANDOM_SENTENCE = "RANDOM_SENTENCE_SPOUT";
 
   public static final String BOLT_SPLIT_SENTENCE = "SPLIT_SENTENCE_BOLT";
@@ -10,4 +12,9 @@ public class Constants {
   public static final String TWEET_TEXT = "TWEET_TEXT";
   public static final String TWEET_METADATA = "TWEET_METADATA";
   public static final String WORD = "WORD";
+
+  public class Configuration {
+    public static final String RUN_ON_LOCAL = "Storm.Topology.Local";
+    public static final String TOPOLOGY_WORKERS = "Storm.Topology.Workers";
+  }
 }
